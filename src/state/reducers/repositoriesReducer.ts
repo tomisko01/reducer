fd098c978c85d758318a83f7495fa3e36c09be5a
tomisko <toom134@gmail.com>
2022-02-23 19:18:26 +0100

    prepare exports and store

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,16 +1,19 @@
+import {Action} from "../actions";
+import {ActionType} from "../action-types";
+
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
-        case 'search_repositories':
+        case ActionType.SEARCH_REPOSITORIES:
             return {loading: true, error: null, data: []};
-        case 'search_repositories_succes':
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
             return {loading: false, error: null, data: action.payload};
-        case 'search_repositories_error':
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
             return {loading: false, error: action.payload, data: []}
         default:
             return state;
